e1d22160e73d614c69880728474dedd968e62945
dhuelsbeck <35932641+dhuelsbeck@users.noreply.github.com>
2018-01-29 12:34:29 -0600

    Add files via upload

---
+++ Pizzas/Pizza.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Net;
+using Newtonsoft.Json;
+
+namespace Pizzas
+{
+    public class Pizza
+    {
+        public bool DownloadFile(string fileName)
+        {
+            try
+            {                
+                using (var client = new WebClient())
+                {
+                    client.DownloadFile("http://files.olo.com/pizzas.json", fileName);
+                }
+                return true;
+            }
+            catch(Exception ex)
+            {
+                return false;
+            }
+        }
+        public List<Order> LoadJson(string file)
+        {
+            var orders = new List<Order>();
+
+            using (StreamReader r = File.OpenText(file))
+            {
+                string json = r.ReadToEnd();
+                orders = JsonConvert.DeserializeObject<List<Order>>(json);
+            }
+
+            return orders;
+        }          
+    }
+
+    public class Order
+    {
+        public string[] toppings;
+    }
+}
---
+++ Pizzas/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+
+namespace Pizzas
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var pizzaList = new Pizza();
+
+            var fileName = Path.Combine(Path.GetTempPath(), "pizzatest.json");
+
+            //here go get the file
+            if (pizzaList.DownloadFile(fileName))
+            {
+                //here load the file
+                var orders = pizzaList.LoadJson(fileName);
+
+                //here show the top 20 toppings
+                var query = orders.SelectMany(x => x.toppings)
+                    .GroupBy(s => s)
+                    .Select(g => new {Name = g.Key, Count = g.Count()})
+                    .OrderByDescending(d => d.Count)
+                    .Take(20);
+
+                foreach (var result in query)
+                {
+                    Console.WriteLine("Topping: {0}, Count: {1}", result.Name, result.Count);
+                }
+            }
+            else
+            {
+                Console.WriteLine("Errors downloading the file!");
+            }
+
+            Console.WriteLine("Press any key to close");
+            Console.ReadKey();
+        }
+    }
+}
